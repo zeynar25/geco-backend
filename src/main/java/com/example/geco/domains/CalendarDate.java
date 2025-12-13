349e6b4cff852656325e703ea8cba28dc6a9e6e0
nate <ryrgnx@gmail.com>
2025-12-13 18:12:38 +0800

    CalendarDate domain made

---
+++ src/main/java/com/example/geco/domains/CalendarDate.java
@@ -0,0 +1,41 @@
+package com.example.geco.domains;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name="calendar_date")
+public class CalendarDate {
+	public enum DateStatus {
+	    AVAILABLE, FULLY_BOOKED, CLOSED
+	}
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Integer dateId;
+	
+	@Column(nullable = false, unique = true)
+	@Temporal(TemporalType.DATE)
+	private Date date;
+	
+	@Enumerated(EnumType.STRING)
+	private DateStatus dateStatus;
+}
