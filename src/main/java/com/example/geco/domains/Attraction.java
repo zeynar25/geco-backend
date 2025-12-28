142f413edca86bb270916a1b826f7ea2808e8dac
nate <ryrgnx@gmail.com>
2025-12-28 18:04:12 +0800

    attraction description text update from varchar(255)

--- src/main/java/com/example/geco/domains/Attraction.java
+++ src/main/java/com/example/geco/domains/Attraction.java
@@ -1,5 +1,6 @@
 package com.example.geco.domains;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -21,6 +22,8 @@ public class Attraction {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer attractionId;
 	private String name;
+	
+	@Column(columnDefinition = "TEXT")
 	private String description;
 	private String funFact;
 	
