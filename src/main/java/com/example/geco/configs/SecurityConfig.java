d3cbfb1310cd57425aca052867bbee71ed6c6f0f
nate <ryrgnx@gmail.com>
2025-12-30 18:08:56 +0800

    SpringSecurity adjusted for CORS

--- src/main/java/com/example/geco/configs/SecurityConfig.java
+++ src/main/java/com/example/geco/configs/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.geco.configs;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -30,6 +31,9 @@ import com.example.geco.services.AccountService;
 public class SecurityConfig {
 	@Autowired
 	private JwtFilter jwtFilter;
+	
+	@Value("#{'${app.cors.allowed-origins}'.split(',')}")
+    private String[] allowedOrigins;
 	
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -130,7 +134,8 @@ public class SecurityConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
-	    configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+	    
+	    configuration.setAllowedOrigins(List.of(allowedOrigins));
 	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(List.of("*"));
 	    configuration.setAllowCredentials(true);
