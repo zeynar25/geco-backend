26523550c08afa834db1f51b1b1266d8d4e03f00
nate <ryrgnx@gmail.com>
2026-02-05 15:20:21 +0800

    restriction name must be unique

--- src/main/java/com/example/geco/domains/Restriction.java
+++ src/main/java/com/example/geco/domains/Restriction.java
@@ -1,5 +1,6 @@
 package com.example.geco.domains;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -20,6 +21,8 @@ public class Restriction {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY) 
 	private Integer id;
+	
+	@Column(unique = true)
 	private String name;
 	private Integer value;
 }
